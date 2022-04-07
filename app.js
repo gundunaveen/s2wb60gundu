e84442e432ec8a8dab008bd0056e100f77bca973
gundunaveen <98118887+gundunaveen@users.noreply.github.com>
2022-04-06 22:13:33 -0500

    updated

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var juiceRouter = require('./routes/juice');
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -21,7 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/juice', juiceRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
